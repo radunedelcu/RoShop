4b2119b40ac6d1634f077b72020983fce6106bcc
radu <nedelcu.radu21@gmail.com>
2022-03-31 19:06:53 +0300

    Made relationship between role and user

--- RoShop/RoShop/Models/Product.cs
+++ RoShop/RoShop/Models/Product.cs
@@ -3,7 +3,7 @@
   public class Product
   {
     public int Id { get; set; }
-    public string ProductName { get; set; }
+    public string Name { get; set; }
     public int Price { get; set; }
     public string Description { get; set; }
   }
--- RoShop/RoShop/Models/Role.cs
+++ RoShop/RoShop/Models/Role.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RoShop.Models
+﻿namespace RoShop.Models
 {
-    public class Role
-    {
-        public int Id { get; set; }
-        public string NormalUser { get; set; }
-        public string Administrator { get; set; }
-    }
+  public class Role
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+  }
 }
