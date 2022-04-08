9639a4f26dd2d980562f1a00653c974b95252028
radu <nedelcu.radu21@gmail.com>
2022-04-08 17:52:01 +0300

    added product controller and create product

--- RoShop/RoShop/Controllers/ProductController.cs
+++ RoShop/RoShop/Controllers/ProductController.cs
@@ -1,15 +1,54 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using RoShop.Data;
+using RoShop.Models;
 
 namespace RoShop.Controllers
 {
-  [Authorize(Roles = "user")]
+  [Authorize(Roles = "admin")]
 
   public class ProductController : Controller
   {
+    private readonly ApplicationDbContext _context;
+
+    public ProductController(ApplicationDbContext applicationDbContext)
+    {
+      _context = applicationDbContext;
+    }
     public IActionResult Index()
     {
+      IEnumerable<Product> objList = _context.Product;
+      return View(objList);
+    }
+
+    [HttpGet]
+    public IActionResult Create()
+    {
       return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+
+    public IActionResult Create(Product obj)
+    {
+      if (ModelState.IsValid)
+      {
+        _context.Product.Add(obj);
+        _context.SaveChanges();
+
+        //add UserProduct in database
+        UserProduct userProduct = new UserProduct();
+        string email = HttpContext.User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Email)?.Value;
+        User user = _context.User.Where(a => a.Email == email).SingleOrDefault();
+        userProduct.IdUser = user.Id;
+        userProduct.User = user;
+        return RedirectToAction("Index");
+      }
+      return View(obj);
     }
   }
 }
